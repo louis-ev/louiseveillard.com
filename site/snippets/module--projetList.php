4f1b0d81561a5afab5d3002b5325477ed64df561
Louis Eveillard <louis.eveillard@gmail.com>
2016-03-01 16:35:24 +0100

    nouvelle catégorie / nouveau projet

--- site/snippets/module--projetList.php
+++ site/snippets/module--projetList.php
@@ -4,6 +4,7 @@
 	<h3 class="isotope--item is--hidden" data-type="vr" data-num="100">Projets en réalité virtuelle</h3>
 	<h3 class="isotope--item is--hidden" data-type="objets-interactifs" data-num="100">Installations interactives</h3>
 	<h3 class="isotope--item is--hidden" data-type="outils-pedagogiques" data-num="100">Outils pédagogiques</h3>
+	<h3 class="isotope--item is--hidden" data-type="autres" data-num="100">Autres projets</h3>
   <?php foreach($pages->findByURI('projets')->children()->visible() as $p): ?>
     <?php $index = str_pad( $p->num(), 2, '0', STR_PAD_LEFT); ?>
     <li class="module--projetList--projetName isotope--item <?php e($p->isOpen(), 'is--active') ?>" data-index="<?= $index ?>" data-type="<?= str::slug( $p->type()); ?>" >
