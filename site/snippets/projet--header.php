01dc6d266c1dd8d83c68053ae3bda0faf889796a
Louis Eveillard <louis.eveillard@gmail.com>
2016-03-05 20:02:39 +0100

    maj aspect menu

--- site/snippets/projet--header.php
+++ site/snippets/projet--header.php
@@ -27,10 +27,6 @@
 	  $image = $p->imageCover()->toFile();
 ?>
 <a class="module--projet--visuel" href="<?= $image->url() ?>" target="_blank">
-
-<!--   <div class="module--projet--visuel--inside lazyload" data-sizes="auto" data-bgset="//localhost:8888/louiseveillard.com-v2/thumbs/point-a-point_puces-typo_sorties-2-25f4b9838e0bba92c6706d1084933f91.jpg 300w, //localhost:8888/louiseveillard.com-v2/thumbs/point-a-point_puces-typo_sorties-2-2df06d4e567df22bef064fca78ee896c.jpg 600w, //localhost:8888/louiseveillard.com-v2/thumbs/point-a-point_puces-typo_sorties-2-ec4ababbda4ed1f2a9d467cce9251fe6.jpg 1200w, //localhost:8888/louiseveillard.com-v2/thumbs/point-a-point_puces-typo_sorties-2-61240d108f3dae67e0a893a1ad94057f.jpg 1600w, //localhost:8888/louiseveillard.com-v2/thumbs/point-a-point_puces-typo_sorties-2-4a28150fb1318aaf4060c0b4455826be.jpg 2800w"></div> -->
-
-
   <?php
 		$srcsetstring = kirbytag(array(
 		  'image'  				=> $image->filename(),
@@ -39,9 +35,7 @@
 		  'return_srcset' => true
 		));
   ?>
-  <div class="module--projet--visuel--inside lazyload" data-sizes="auto" data-bgset="<?= $srcsetstring; ?>">
-  </div>
-
+  <div class="module--projet--visuel--inside lazyload" data-sizes="auto" data-bgset="<?= $srcsetstring; ?>"></div>
 </a>
 <?php
   endif;
